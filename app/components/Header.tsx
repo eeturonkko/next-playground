a2c9977fda0ad8e04f1685039b211c3de6eb944b
Eetu Rönkkö <eetu.kristian.ronkko@gmail.com>
2023-05-28 15:47:03 +0300

    Changed a tag to link tags in CTA button & header

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -7,7 +7,7 @@ function Header() {
   return (
     <nav className=" border-gray-200 bg-gray-900 py-2">
       <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
-        <a href="https://flowbite.com/" className="flex items-center">
+        <Link href="/" className="flex items-center">
           <img
             src="https://flowbite.com/docs/images/logo.svg"
             className="h-8 mr-3"
@@ -16,7 +16,7 @@ function Header() {
           <span className="self-center text-2xl font-semibold whitespace-nowrap text-white">
             DataCollector
           </span>
-        </a>
+        </Link>
 
         <div className="hidden w-full md:block md:w-auto" id="navbar-default">
           <ul className="font-medium flex flex-col p-4 md:p-0 mt-4 border rounded-lg md:flex-row md:space-x-8 md:mt-0 md:border-0 md:bg-gray-900 border-gray-700">
