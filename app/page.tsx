0cf8065641e004a5345b989d00660d399ad961e5
Eetu Rönkkö <eetu.kristian.ronkko@gmail.com>
2023-05-31 21:14:58 +0300

    added a cool ass wave to front page!

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import GradientButton from "./components/GradientButton";
 export default function HomePage() {
   return (
     <>
-      <main className="flex min-h-screen flex-col items-center justify-between p-24 ">
+      <main className="relative flex h-screen flex-col items-center justify-between p-24 ">
         <section>
           <h2 className="text-7xl font-bold text-white">
             You <span className="text-blue-500">submit</span> we
@@ -17,6 +17,17 @@ export default function HomePage() {
             <GradientButton text="Get Started" path="/sign-in" />
           </div>
         </section>
+        <svg
+          className="absolute bottom-0 left-0 w-full"
+          xmlns="http://www.w3.org/2000/svg"
+          viewBox="0 0 1440 320"
+        >
+          <path
+            fill="#0099ff"
+            fill-opacity="1"
+            d="M0,224L48,213.3C96,203,192,181,288,144C384,107,480,53,576,69.3C672,85,768,171,864,186.7C960,203,1056,149,1152,144C1248,139,1344,181,1392,202.7L1440,224L1440,320L1392,320C1344,320,1248,320,1152,320C1056,320,960,320,864,320C768,320,672,320,576,320C480,320,384,320,288,320C192,320,96,320,48,320L0,320Z"
+          ></path>
+        </svg>
       </main>
     </>
   );
