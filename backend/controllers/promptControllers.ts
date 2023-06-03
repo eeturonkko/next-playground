2cb64107bf5c7abac5c8a9b6b73b124b67c95f33
Eetu <eetu.kristian.ronkko@gmail.com>
2023-06-03 13:50:13 +0300

    added Prompts  & working on prompt controller

--- backend/controllers/promptControllers.ts
+++ backend/controllers/promptControllers.ts
@@ -1,9 +1,22 @@
 import { Request, Response } from "express";
+require("dotenv").config();
 const { PrismaClient } = require("@prisma/client");
 const prisma = new PrismaClient();
+const openai = require("openai");
+
+openai.apiKey = process.env.OPENAI_API_KEY;
 
 async function createIdeas(req: Request, res: Response) {
-  console.log("hi mom");
+  const { prompt } = req.body;
+  const gptResponse = await openai.complete({
+    engine: "davinci",
+    prompt: prompt,
+    maxTokens: 60,
+    temperature: 0.9,
+  });
+
+  const { choices } = gptResponse.data;
+  console.log(choices);
 }
 
 export { createIdeas };
---
+++ backend/prisma/migrations/20230603104623_added/migration.sql
@@ -0,0 +1,6 @@
+-- CreateTable
+CREATE TABLE "Prompts" (
+    "id" TEXT NOT NULL PRIMARY KEY,
+    "prompt" TEXT NOT NULL,
+    "userId" TEXT NOT NULL
+);
