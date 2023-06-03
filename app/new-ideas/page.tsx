460f808a665c8e6bc8c52c4b1c0b150db2dc0184
Eetu <eetu.kristian.ronkko@gmail.com>
2023-06-03 13:00:07 +0300

    Working on new ideas page

---
+++ app/new-ideas/page.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+function NewIdeasPage() {
+  return (
+    <>
+      <main className="h-screen text-white">
+        <h1 className="text-4xl">New Ideas</h1>
+      </main>
+    </>
+  );
+}
+
+export default NewIdeasPage;
