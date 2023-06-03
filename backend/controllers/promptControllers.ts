85438637818a18a3984907e9a6a0b5dc399e854c
Eetu <eetu.kristian.ronkko@gmail.com>
2023-06-03 14:15:31 +0300

    updated Promps model

--- backend/controllers/promptControllers.ts
+++ backend/controllers/promptControllers.ts
@@ -7,16 +7,30 @@ const openai = require("openai");
 openai.apiKey = process.env.OPENAI_API_KEY;
 
 async function createIdeas(req: Request, res: Response) {
-  const { prompt } = req.body;
-  const gptResponse = await openai.complete({
-    engine: "davinci",
-    prompt: prompt,
-    maxTokens: 60,
-    temperature: 0.9,
-  });
+  const { prompt, userId } = req.body;
+  try {
+    const gptResponse = await openai.Completion.create({
+      engine: "davinci-codex",
+      prompt: prompt,
+      max_tokens: 60,
+      temperature: 0.9,
+    });
 
-  const { choices } = gptResponse.data;
-  console.log(choices);
+    const { choices } = gptResponse.data;
+    const choicesForDb = choices.map((choice: any) => choice.text);
+    const newPrompt = await prisma.prompts.create({
+      data: {
+        prompt,
+        userId: userId,
+        choices: JSON.stringify(choicesForDb),
+      },
+    });
+    console.log(choicesForDb);
+    res.json(newPrompt);
+  } catch (error) {
+    console.error(error);
+    res.status(500).send("Server Error");
+  }
 }
 
 export { createIdeas };
---
+++ backend/prisma/migrations/20230603111458_updated/migration.sql
@@ -0,0 +1,2 @@
+-- AlterTable
+ALTER TABLE "Prompts" ADD COLUMN "choices" TEXT;
