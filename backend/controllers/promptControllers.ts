8b327170dd05e49ea44957ed69a9f9265c8f59b4
Eetu <eetu.kristian.ronkko@gmail.com>
2023-06-03 14:58:37 +0300

    openAI api call works. Working on saving it to db

--- backend/controllers/promptControllers.ts
+++ backend/controllers/promptControllers.ts
@@ -1,35 +1,38 @@
 import { Request, Response } from "express";
-require("dotenv").config();
 const { PrismaClient } = require("@prisma/client");
 const prisma = new PrismaClient();
-const openai = require("openai");
+import { Configuration, OpenAIApi } from "openai";
 
-openai.apiKey = process.env.OPENAI_API_KEY;
+const configuration = new Configuration({
+  apiKey: process.env.OPENAI_API_KEY,
+});
+const openai = new OpenAIApi(configuration);
 
 async function createIdeas(req: Request, res: Response) {
   const { prompt, userId } = req.body;
-  try {
-    const gptResponse = await openai.Completion.create({
-      engine: "davinci-codex",
-      prompt: prompt,
-      max_tokens: 60,
-      temperature: 0.9,
-    });
 
-    const { choices } = gptResponse.data;
-    const choicesForDb = choices.map((choice: any) => choice.text);
-    const newPrompt = await prisma.prompts.create({
-      data: {
-        prompt,
-        userId: userId,
-        choices: JSON.stringify(choicesForDb),
+  if (!configuration.apiKey) {
+    res.status(500).json({
+      error: {
+        message: "OpenAI API key not configured.",
       },
     });
-    console.log(choicesForDb);
-    res.json(newPrompt);
+    return;
+  }
+
+  try {
+    const completion = await openai.createCompletion({
+      model: "text-davinci-003",
+      prompt: prompt,
+      temperature: 0,
+      max_tokens: 100,
+      top_p: 1,
+      frequency_penalty: 0.0,
+      presence_penalty: 0.0,
+    });
+    res.status(200).json({ result: completion.data.choices[0].text });
   } catch (error) {
-    console.error(error);
-    res.status(500).send("Server Error");
+    res.status(500).send("Something went wrong.");
   }
 }
 
